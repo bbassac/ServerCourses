ae962eed0139aab0b6e9b39959eef113d713b2cf
Bruno Bassac <b.bassac@lectra.com>
2019-02-13 16:19:58 +0100

    Changed to swagger

--- src/main/java/courses/Start.java
+++ src/main/java/courses/Start.java
@@ -1,6 +1,5 @@
 package courses;
 
-import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  */
 @SpringBootApplication
 @EnableJpaRepositories
-@EnableJSONDoc
 public class Start {
 
     public static void main(String[] args) throws Exception {
